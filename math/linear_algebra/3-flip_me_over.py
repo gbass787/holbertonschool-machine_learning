cf65ca1589a56c1256d26fcd017d550f85660689
gbass787 <4551@holbertonschool.com>
2023-03-22 01:01:39 +0000

    fixed issues in task 3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
+
 def matrix_transpose(matrix):
     """
     Returns the transpose of a 2D matrix.
 
     Args:
-    matrix (list of lists): 2D matrix
+        matrix (list of lists): 2D matrix
 
     Returns:
-    list of lists: Transposed matrix
+        list of lists: Transposed matrix
     """
     num_rows = len(matrix)
     num_cols = len(matrix[0])
-    transposed = [[matrix[j][i] for j in range(num_rows)] for i in range(num_cols)]
+    transposed = []
+    for i in range(num_cols):
+        transposed.append([matrix[j][i] for j in range(num_rows)])
     return transposed
