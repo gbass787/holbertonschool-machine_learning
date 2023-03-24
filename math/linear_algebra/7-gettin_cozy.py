fb39868eb4e60aa94cb115b12c6ee823b288d749
gbass787 <4551@holbertonschool.com>
2023-03-24 22:31:56 +0000

    fixed pycodestyle issues

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -2,28 +2,30 @@
 """
 Concatenates two matrices along a specific axis.
 """
+
+
 def cat_matrices2D(mat1, mat2, axis=0):
     """
     Args:
         mat1 (list[list]): A 2D matrix containing ints/floats
         mat2 (list[list]): A 2D matrix containing ints/floats
-        axis (int): The axis along which to concatenate the matrices. 
-            If axis=0, concatenate rows; if axis=1, concatenate columns. 
+        axis (int): The axis along which to concatenate the matrices.
+            If axis=0, concatenate rows; if axis=1, concatenate columns.
             Default is 0.
 
     Returns:
-        A new 2D matrix containing the concatenated matrices, or None if the 
+        A new 2D matrix containing the concatenated matrices, or None if the
         matrices cannot be concatenated along the specified axis.
 
     Raises:
         None
     """
-    # Ensure that mat1 and mat2 have the same shape in the non-concatenated axis
+    #Ensure that mat1 and mat2 have the same shape in the non-concatenated axis
     if axis == 0 and len(mat1[0]) != len(mat2[0]):
         return None
     elif axis == 1 and len(mat1) != len(mat2):
         return None
-    
+
     # Concatenate the matrices along the specified axis
     if axis == 0:
         return [row for row in mat1] + [row for row in mat2]
