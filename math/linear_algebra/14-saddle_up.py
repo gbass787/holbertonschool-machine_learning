e570b7490435ba5e6b86b8d334ff8cbe55f85c81
gbass787 <4551@holbertonschool.com>
2023-03-25 02:31:38 +0000

    ...

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 import numpy as np
 """
-performs matrix multiplication of two numpy ndarrays.
+Returns the matrix product of two arrays.
 """
 
 
-def np_matmul(mat1, mat2):
+def np_matmul(mat1: np.ndarray, mat2: np.ndarray) -> np.ndarray:
     """
-    Performs matrix multiplication between two numpy ndarrays
-
     Args:
-    - mat1: numpy ndarray of shape (m, n)
-    - mat2: numpy ndarray of shape (n, p)
-
+        mat1 (numpy.ndarray): The first matrix.
+        mat2 (numpy.ndarray): The second matrix.
     Returns:
-    - numpy ndarray of shape (m, p) containing the
-result of matrix multiplication
+        numpy.ndarray: The product of the two matrices.
+    Raises:
+        ValueError: If the matrices have incompatible dimensions.
     """
-    return np.sum(mat1[..., np.newaxis] * mat2.T, axis=-2)
+    return np.matmul(mat1, mat2)
