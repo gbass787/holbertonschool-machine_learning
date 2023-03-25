cafc64802c6ed3034f3999afdfdbad749fcef020
gbass787 <4551@holbertonschool.com>
2023-03-25 02:25:52 +0000

    fixing task 14

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -2,28 +2,18 @@
 """
 performs matrix multiplication of two numpy ndarrays.
 """
-import numpy as np
-"""
-Returns the matrix product of two arrays.
-"""
 
 
 def np_matmul(mat1, mat2):
     """
-    Performs matrix multiplication of two numpy ndarrays.
+    Performs matrix multiplication between two numpy ndarrays
 
     Args:
-        mat1 (numpy.ndarray): A numpy ndarray representing the first matrix.
-        mat2 (numpy.ndarray): A numpy ndarray representing the second matrix.
+    - mat1: numpy ndarray of shape (m, n)
+    - mat2: numpy ndarray of shape (n, p)
 
     Returns:
-        A new numpy.ndarray representing the product of the two input matrices.
-
-    Raises:
-        ValueError: If the matrices cannot be multiplied.
+    - numpy ndarray of shape (m, p) containing the
+result of matrix multiplication
     """
-    # Check if matrices can be multiplied using the dot product shape rule
-    assert mat1.shape[1] == mat2.shape[0], "Matrices cannot be multiplied"
-
-    # Compute the product using np.dot and return the result
-    return np.dot(mat1, mat2)
+    return mat1 @ mat2
