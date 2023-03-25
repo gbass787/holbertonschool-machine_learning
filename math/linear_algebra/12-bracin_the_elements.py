45a12f4f7bf5cb29526c7360b48550d0e8ecbd5f
gbass787 <4551@holbertonschool.com>
2023-03-25 02:21:44 +0000

    fixed task 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -6,27 +6,19 @@ and division on mat1 and mat2.
 
 
 def np_elementwise(mat1, mat2):
-    # Get the shape of the matrices
-    shape = mat1.shape
+    """Perform element-wise addition, sub, multiplication, and division.
 
-    # Reshape the matrices as 1D arrays
-    mat1_1d = mat1.reshape(-1)
-    mat2_1d = mat2.reshape(-1)
+    Args:
+        mat1 (numpy.ndarray): The first matrix.
+        mat2 (numpy.ndarray): The second matrix.
 
-    # Element-wise sum
-    sum_mat_1d = [mat1_1d[i] + mat2_1d[i] for i in range(len(mat1_1d))]
-    sum_mat = np.array(sum_mat_1d).reshape(shape)
+    Returns:
+        tuple: A tuple containing the element-wise sum, difference, product,
+        and quotient, respectively.
+    """
+    sum_arr = mat1 + mat2
+    diff_arr = mat1 - mat2
+    prod_arr = mat1 * mat2
+    quotient_arr = mat1 / mat2
 
-    # Element-wise difference
-    diff_mat_1d = [mat1_1d[i] - mat2_1d[i] for i in range(len(mat1_1d))]
-    diff_mat = np.array(diff_mat_1d).reshape(shape)
-
-    # Element-wise product
-    prod_mat_1d = [mat1_1d[i] * mat2_1d[i] for i in range(len(mat1_1d))]
-    prod_mat = np.array(prod_mat_1d).reshape(shape)
-
-    # Element-wise quotient
-    quo_mat_1d = [mat1_1d[i] / mat2_1d[i] for i in range(len(mat1_1d))]
-    quo_mat = np.array(quo_mat_1d).reshape(shape)
-
-    return sum_mat, diff_mat, prod_mat, quo_mat
+    return (sum_arr, diff_arr, prod_arr, quotient_arr)
