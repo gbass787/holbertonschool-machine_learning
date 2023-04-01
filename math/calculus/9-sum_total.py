05b9cabf219920cf7d92c7fcd2c817e93d6ba012
gbass787 <4551@holbertonschool.com>
2023-04-01 02:01:53 +0000

    fix task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
 def summation_i_squared(n):
-"""
-    Calculates the summation of i^2 for i from 1 to n.
+    """
+    Calculates the sum of the squares of the first n positive integers.
 
-    Parameters:
-        n (int): The stopping condition for the summation. Must be a positive integer.
+    Args:
+    - n: an integer representing the stopping condition
 
     Returns:
-        int: The integer value of the summation.
-
-        If n is not a valid number, returns None.
-"""
+    - an integer representing the sum of the squares of the first n positive integers,
+      or None if n is not a positive integer
+    """
     if not isinstance(n, int) or n <= 0:
         return None
-    return sum(i ** 2 for i in range(1, n+1))
+    return int(n * (n + 1) * (2 * n + 1) / 6)
