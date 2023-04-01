6bf56d6557194ea39437ba93ab71caaadf5ae71f
gbass787 <4551@holbertonschool.com>
2023-04-01 01:26:17 +0000

    task 9 added documentation

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
 def summation_i_squared(n):
+"""
+    Calculates the summation of i^2 for i from 1 to n.
+
+    Parameters:
+        n (int): The stopping condition for the summation. Must be a positive integer.
+
+    Returns:
+        int: The integer value of the summation.
+
+        If n is not a valid number, returns None.
+"""
     if not isinstance(n, int) or n <= 0:
         return None
     return sum(i ** 2 for i in range(1, n+1))
