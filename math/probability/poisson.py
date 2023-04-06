383181ca66b478aceb35af811dd7fd63d8953e6e
gbass787 <4551@holbertonschool.com>
2023-04-06 22:27:05 +0000

    task 1

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -10,33 +10,33 @@ class Poisson:
     """
 
     def __init__(self, data=None, lambtha=1.):
-        """Poisson class constructor"""
-        self.lambtha = float(lambtha)
-
+        """Initialize Poisson class"""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
-            if len(data) < 2:
+            elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
+            else:
+                self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
-        """Calculates the value of the PMF for a given number of “successes”"""
+        """Calculate the value of the PMF for a given number of successes"""
         if not isinstance(k, int):
             k = int(k)
-
         if k < 0:
             return 0
-
-        factorial_k = 1
-        for i in range(1, k+1):
-            factorial_k *= i
-
-        pmf_value = ((e ** -self.lambtha) * (self.lambtha ** k)) / factorial_k
-        return pmf_value
+        else:
+            factorial_k = 1
+            for i in range(1, k + 1):
+                factorial_k *= i
+            pmf_value = ((2.7182818285 ** -self.lambtha) *
+                         (self.lambtha ** k)) / factorial_k
+            return pmf_value
 
     def cdf(self, k):
         """Calculates the value of the CDF for a given number of successes"""
