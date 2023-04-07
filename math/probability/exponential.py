6b7aa2fbb52ab66542a75e530fe5049556413023
gbass787 <4551@holbertonschool.com>
2023-04-07 00:53:46 +0000

    fixing documentation issues

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 """
-Exponential Exponential Exponential
+This module defines the Exponential class, which represents
+an exponential distribution.
 """
 
 
 class Exponential:
     def __init__(self, data=None, lambtha=1.):
         """
-        Represents an exponential distribution.
+        Initializes an instance of the Exponential class.
 
         Args:
-            data (list): A list of the data to be used to estimate
-the distribution.
+            data (list): A list of data points used to estimate the
+distribution. Defaults to None.
+            lambtha (float): The expected number of occurrences in a
+given time frame. Defaults to 1.
+
+        Attributes:
             lambtha (float): The expected number of occurrences in
 a given time frame.
 
-        Sets the instance attribute:
-            lambtha (float): The expected number of occurrences in a
-given time frame.
-
         Raises:
-            ValueError: If lambtha is not a positive value and data is
-not given.
+            ValueError: If lambtha is not a positive value and data
+is not given.
             TypeError: If data is not a list.
             ValueError: If data does not contain at least two data points.
         """
