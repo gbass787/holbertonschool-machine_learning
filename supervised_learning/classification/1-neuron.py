5f911601ec236c76436e9fb8feca512ad49e4947
gbass787 <4551@holbertonschool.com>
2023-04-20 05:26:32 +0000

    task 1 fix

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -29,15 +29,15 @@ class Neuron:
         self.__W = np.random.normal(size=(1, nx))
         self.__b = 0
         self.__A = 0
-        
+
     @property
     def W(self):
         return self.__W
-    
+
     @property
     def b(self):
         return self.__b
-    
+
     @property
     def A(self):
         return self.__A
