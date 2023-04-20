0a84abfcc1a3d05f45db0cdb5831dc7b41726855
gbass787 <4551@holbertonschool.com>
2023-04-20 00:25:13 +0000

    task 0

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,33 +1,55 @@
 #!/usr/bin/env python3
-"""
-Module for Neuron class
-"""
+"""Module that defines a single neuron performing binary classification"""
 
 import numpy as np
 
 
 class Neuron:
-    """
-    Class that defines a single neuron performing binary classification
-    """
+    """Class that defines a single neuron performing binary classification"""
+
     def __init__(self, nx):
-        """
-        Constructor method for Neuron class
+        """Class constructor for Neuron
+
         Args:
             nx (int): The number of input features to the neuron
-        Attributes:
-            W (numpy.ndarray): The weights vector for the neuron
-            b (float): The bias for the neuron
-            A (float): The activated output of the neuron (prediction)
+
         Raises:
             TypeError: If nx is not an integer
             ValueError: If nx is less than 1
         """
-        if type(nx) is not int:
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         elif nx < 1:
             raise ValueError("nx must be a positive integer")
-        else:
-            self.W = np.random.normal(size=nx)
-            self.b = 0
-            self.A = 0
+
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights vector for the neuron"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias for the neuron"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output of the neuron (prediction)"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron
+
+        Args:
+            X (numpy.ndarray): The input data of shape (nx, m)
+
+        Returns:
+            numpy.ndarray: The activated output of the neuron (prediction)
+        """
+        z = np.matmul(self.W, X) + self.b
+        self.__A = 1 / (1 + np.exp(-z))
+        return self.A
