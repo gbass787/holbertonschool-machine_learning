95425fc747847ca0c29e3ea6453bf85a3b744dd3
gbass787 <4551@holbertonschool.com>
2023-05-08 05:45:52 +0000

    ...

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Error Analysis """
+
+import numpy as np
+
+
+def precision(confusion):
+    """ Calculates the precision for each class in a confusion matrix
+    Argumemts:
+        confusion (numpy.ndarray): confusion matrix of shape (classes, classes)
+            where row indices represent the correct labels
+            and column indices represent the predicted labels
+    Returns:
+        numpy.ndarray: precision of each class
+    """
+    # precision = true_positives / all_positives from the column
+    precision = np.zeros(confusion.shape[0])
+
+    for i in range(confusion.shape[0]):
+        true_positives = confusion[i][i]
+        all_positives = np.sum(confusion[:][i])
+
+        precision[i] = true_positives / all_positives
+
+    return precision
---
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Error Analysis """
+
+import numpy as np
+precision = __import__('2-precision').precision
+sensitivity = __import__('1-sensitivity').sensitivity
+
+
+def f1_score(confusion):
+    """ calculates the F1 score of a confusion matrix
+    Arguments:
+        confusion (numpy.ndarray): confusion matrix of shape (classes, classes)
+            where row indices represent the correct labels
+            and column indices represent the predicted labels
+    Returns:
+        numpy.ndarray: F1 score of each class
+    """
+    # F1 = 2 * (precision * sensitivity) / (precision + sensitivity)
+    return 2 * (precision(confusion) * sensitivity(confusion)) / \
+        (precision(confusion) + sensitivity(confusion))
